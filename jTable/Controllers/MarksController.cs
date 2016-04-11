f9cacf13dc12b5c58d5fc1ef8795cbaaeb07eff7
caofangsheng93 <fangsheng93@163.com>
2016-04-11 13:08:10 +0800

    ..

--- jTable/Controllers/MarksController.cs
+++ jTable/Controllers/MarksController.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
    
-namespace jTable.Controllers   
+namespace jTable.Controllers     
 {
     public class MarksController : Controller
     {
